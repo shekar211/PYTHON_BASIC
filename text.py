d106f41477825cc4286833219034f71afa3f744c
Shekar <nshekar211@gmail.com>
2026-06-25 22:25:19 +1100

    git commands

---
+++ text.py
@@ -0,0 +1,21 @@
+print("hello world","world",sep="#")
+
+x = "10.5"
+print(int(float(x)))
+
+print("\test\ntesting")
+print(r"\test\ntesting")
+print("\\test\\ntesting")
+
+print("""hello
+     my name is
+     John
+      """)
+
+x="heplo"
+print(x[0:3:2]) #start:end:step
+print(x[::-1]) #reverse string
+
+for i in range(0,10,2):
+    if i% == 0:
+        print(i)
